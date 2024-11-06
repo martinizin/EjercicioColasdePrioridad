8a648f2df2337ee3a476c2717e328366f78f7f34
martinizin <mar45pillacela@gmail.com>
2024-11-06 09:23:44 -0500

    Creacion y desarrollo de clases Pacientes y Gestion pacientes

---
+++ src/GestionDePacientes.java
@@ -0,0 +1,22 @@
+import java.util.PriorityQueue;
+
+public class GestionDePacientes {
+    private PriorityQueue<Pacientes>colaPacientes;
+
+    public GestionDePacientes() {
+        colaPacientes = new PriorityQueue<>();
+    }
+
+    public void agregarPaciente(String nombre, String apellido, int prioridad) {
+        Pacientes paciente = new Pacientes(nombre, apellido, prioridad);
+        colaPacientes.add(paciente);
+    }
+
+    public Pacientes atenderPaciente() {
+        return colaPacientes.poll(); //Retornamos el paciente con mayor prioridad en la cola
+    }
+
+    public boolean verificarPacientes() {
+        return !colaPacientes.isEmpty();
+    }
+}
--- src/Pacientes.java
+++ src/Pacientes.java
@@ -1,5 +1,37 @@
-public class Pacientes {
+public class Pacientes implements Comparable<Pacientes> {
     private String Nombre;
     private String Apellido;
     private  int prioridad=0;
+
+    public Pacientes(String nombre, String apellido, int prioridad) {
+        Nombre = nombre;
+        Apellido = apellido;
+        this.prioridad = prioridad;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    @Override
+    public int compareTo(Pacientes otro) {
+        return Integer.compare(this.prioridad, otro.prioridad);
+    }
+
+
+    @Override
+    public String toString() {
+        return Nombre + " " + Apellido + " (Prioridad: " + prioridad + ")";
+    }
 }
+
+
+
